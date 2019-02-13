84a111abbbebd7be0ebdf0d64a7037e568ca6c82
Vazquez Alferez <Sofia.VazquezAlferez@unimaas.nl>
2019-02-13 11:47:52 +0100

    nothing

--- All Packages installation script.R
+++ All Packages installation script.R
@@ -1,3 +1,4 @@
 #installs all packages needed
 install.packages(c("tidyverse","tidytext","ggwordcloud","topicmodels","lemon","ggthemes","rlang","ldatuning"))#pillar 2
 install.packages(c("arules","arulesSequences"))#pillar 1
+
