53f5ce13d0452ea9131d9c29a3d1de4022b9c7eb
Vazquez Alferez <Sofia.VazquezAlferez@unimaas.nl>
2019-02-13 11:46:10 +0100

    creates script to install all packages used in the project

---
+++ All Packages installation script.R
@@ -0,0 +1,3 @@
+#installs all packages needed
+install.packages(c("tidyverse","tidytext","ggwordcloud","topicmodels","lemon","ggthemes","rlang","ldatuning"))#pillar 2
+install.packages(c("arules","arulesSequences"))#pillar 1
