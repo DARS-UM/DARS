1b2a77766ee0b1aa8bceb4e91f3a61371e618cc5
Raphael Morsomme <rmorsomme@gmail.com>
2019-02-19 14:59:09 +0100

    include a second tabPanel for course recommendation

--- App/Recommender System/ui.R
+++ App/Recommender System/ui.R
@@ -3,7 +3,7 @@
 # set up
 
 # libraries
-library(dplyr)
+library(tidyverse)
 library(shiny)
 load("rules.RDATA")
 load("data_pillar_1.RDATA")
@@ -52,45 +52,109 @@ augment_student_transcript <- function(transcript){
 
 #
 # ui
-fluidPage(
+navbarPage(
   
   # App title
-  titlePanel("Recommender System"),
+  title = "Recommender System",
   
-  # Sidebar layout
-  sidebarLayout(
+  tabPanel(
     
-    # Sidebar panel for inputs
-    sidebarPanel(
+    # Panel Title
+    title = "Red Flag",
+    
+    # Sidebar layout
+    sidebarLayout(
       
-      # Input: student id number
-      textInput(
-        inputId = "student",
-        label   = "Student ID",
-        value   = "6087587"
+      # Sidebar panel for inputs
+      sidebarPanel(
+        
+        # Input: student id number
+        textInput(
+          inputId = "student",
+          label   = "Student ID",
+          value   = "6087587"
+          ),
+        
+        # Input: button for the courses students wants to take following semester
+        checkboxGroupInput(
+          inputId  = "course_chosen",
+          label    = "Tentative Courses for following period",
+          choices  = course_following_semester,
+          selected = course_following_semester,
+          inline   = TRUE
+          )
+        
         ),
       
-      # Input: button for the courses students wants to take following semester
-      checkboxGroupInput(
-        inputId  = "course_chosen",
-        label    = "Tentative Courses for following period",
-        choices  = course_following_semester,
-        selected = course_following_semester,
-        inline   = TRUE
-        )
-      
-      ),
-    
-    # Main panel for displaying outputs
-    mainPanel(
-      
-      # Output: dataset
-      htmlOutput(
-        outputId = "red_flags"
+      # Main panel for displaying outputs
+      mainPanel(
+        
+        # Output: dataset
+        htmlOutput(
+          outputId = "red_flags"
+          )
+        
         )
       
       )
     
+    ),
+  
+  tabPanel(
+    
+    # Panel Title
+    title = "Course Recommender",
+    
+    # Sidebar layout
+    sidebarLayout(
+      
+      # Sidebar panel for inputs
+      sidebarPanel(
+        
+        # Input: button for the courses students wants to take following semester
+        checkboxGroupInput(
+          inputId  = "key_words",
+          label    = "Academic Interest",
+          choices  = c("List", "of", "Key", "Words"),
+          inline   = TRUE
+          ),
+        
+        # Input: additional key word 1-5
+        textInput(
+          inputId = "key_word_1",
+          label   = "Additional Key Word 1"
+          ),
+        textInput(
+          inputId = "key_word_2",
+          label   = "Additional Key Word 2"
+          ),
+        textInput(
+          inputId = "key_word_3",
+          label   = "Additional Key Word 3"
+          ),
+        textInput(
+          inputId = "key_word_4",
+          label   = "Additional Key Word 4"
+          ),
+        textInput(
+          inputId = "key_word_5",
+          label   = "Additional Key Word 5"
+          )
+        
+      ),
+      
+      # Main panel for displaying outputs
+      mainPanel(
+        
+        # Output: dataset
+        htmlOutput(
+          outputId = "course_recommendation"
+        )
+        
+      )
+      
     )
+    
+  )
   
   )
