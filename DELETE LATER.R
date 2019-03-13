7a03289f77ae11317956668e4579501b0e887c0b
Vazquez Alferez <Sofia.VazquezAlferez@unimaas.nl>
2019-03-13 11:01:36 +0100

    recovery script

--- DELETE LATER.R
+++ DELETE LATER.R
@@ -1,4 +1,10 @@
+#saved in workspace
 load("Output/LDA_ntopics.RDATA")
 
+save(result_manual, result_overview , result_description,
+     file = "Output/LDA_ntopics_Find.RDATA")
+
+#recovery
+load("Output/LDA_ntopics_Find.RDATA")
 save(result_manual, result_overview , result_description,
      file = "Output/LDA_ntopics.RDATA")
