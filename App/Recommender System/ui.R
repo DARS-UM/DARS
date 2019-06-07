4b1c93d426aa105f5d4c15f580877c7381fc7986
Sofía  Vázquez Alférez <sofivazquez@gmail.com>
2019-06-07 11:31:33 +0200

    User unterface divide into two sections

--- App/Recommender System/ui.R
+++ App/Recommender System/ui.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:341cb6a7d0897a2932184696aa1996fc234fc207db7552caa5e3f1ebf44d3d01
-size 4568
+oid sha256:c8b3efb23c7205966ce4de01dffadbc4e364582c4e1a5d0b4f366df51da43956
+size 7523
